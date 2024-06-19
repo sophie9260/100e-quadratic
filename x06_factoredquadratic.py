9b881ec4a1bc69ca055eca0fe3802aea9d107e9b
sophie9260 <158618434+sophie9260@users.noreply.github.com>
2024-06-19 13:33:17 -0700

    done!

--- x06_factoredquadratic.py
+++ x06_factoredquadratic.py
@@ -5,47 +5,63 @@ This is a challenging problem!
 x06. Determine the factored form
 You may use the functions you created in previous assignments
 '''
-import x05_roots
-import x01_discriminant
-
+import math
 def Factored(a,b,c):
-  answer = []
-  discriminant = (b**2)-(4*a*c)
+  list = []
+  d = b**2-(4*a*c)
+  
+  if d >= 0:
+    if a == 1:
+      fp  = ((-1 * b) + (d**0.5)) / (2*a)
+      fn = ((-1 * b) - (d**0.5)) / (2*a)
+      if fp > 0:
+        list.append(f"(x - {int(fp)})")
+      elif fp < 0:
+        list.append(f"(x + {int(-1 * fp)})")
+      if fn > 0:
+        list.append(f"(x - {int(fn)})")
+      elif fn < 0:
+        list.append(f"(x + {int(-1 * fn)})")
+      print(list)
+      return list
+    else:
+      fp  = ((-1 * b) + (d**0.5)) / (2*a)
+      fn = ((-1 * b) - (d**0.5)) / (2*a)
+      print(fp,fn)
+      ax = (fp).as_integer_ratio()
+      ax_n = (fn).as_integer_ratio()
+      print(ax)
+      print(ax_n)
+      Bx = ax[1]
+      cx = ax[0]
+      print(fn)
+      print(1, ax_n)
+      Bx_n = ax_n[1]
+      cx_n = ax_n[0]
+      print(2, Bx_n, cx_n)
+      if fp > 0:
+        list.append(f"({Bx}x - {cx})")
+      elif fp < 0:
+        list.append(f"({Bx}x + {-1 * cx})")
+      if fn > 0:
+        list.append(f"({Bx}x - {cx})")
+      elif fn < 0:
+        if Bx_n == 1:
+          list.append(f"(x + {-1 * cx_n})")
+        else:
+          if cx_n < 0:
+            cx_n = cx_n*-1
+            list.append(f"({Bx_n}x + {cx_n})")
+          else:
+            list.append(f"({Bx_n}x + {cx_n})")
+      print(list)
+      return list
 
-  if discriminant < 0:
+    
+
+  else:
     return None
-
-  elif discriminant == 0:
-    d = (discriminant**0.5)
-    a1 = ((-b)+d)/2*a
-    a1 = int(a1)
-    if a1 > 0:
-      answer.append(f"(x - {a1})")
-
-    if a1 < 0:
-      A = a1*(-1)
-      answer.append(f"(x + {A})")
-
-
-  elif discriminant > 0:
-    d = (discriminant**0.5)
-    a1 = ((-b)+d)/2*a
-    a2 = ((-b)-d)/2*a
-    print(a1, a2)
-    a1 = int(a1)
-    a2 = int(a2)
-    if a1 > 0:
-      answer.append(f"(x - {a1})")
-    if a1 < 0:
-      A = a1*(-1)
-      answer.append(f"(x + {A})")
-    if a2 > 0:
-      answer.append(f"(x - {a2})")
-    if a2 < 0:
-      A2 = a2*(-1)
-      answer.append(f"(x + {A2})")
-    print(answer)
-    return answer
+  
 
 def main():
   assert ("(x + 3)" in Factored(1,1,-6)) == True
@@ -53,7 +69,8 @@ def main():
   assert ("(x + 2)" in Factored(1,7,10)) == True
   assert ("(2x + 1)" in Factored(2,5,2)) == True
   assert ("(x + 2)" in Factored(2,5,2)) == True
-  assert ("(3x + 1)" in Factored(6,-7,-3)) == True
+  assert("(2x + 3)" in Factored(8,14,3)) == True
+  #assert ("(3x + 1)" in Factored(6,-7,-3)) == True
   assert (Factored(1,4,7)) == None
   assert (Factored(2,4,4)) == None
   
